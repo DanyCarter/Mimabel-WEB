d16f2b089b721b60ea845d8f4f83aa40ecd578c9
Daniel <danielcadenas88@gmail.com>
2024-05-21 13:44:21 +0200

    Agregando cantidad al producto del carrito

--- src/components/ShoppingCartItem.vue
+++ src/components/ShoppingCartItem.vue
@@ -1,25 +1,32 @@
 <script setup>
-    import { formatCurrency } from '@/helpers';
-    defineProps({
-        item: {
-            type:Object
-        }
-    })
+import { formatCurrency } from '@/helpers';
+defineProps({
+  item: {
+    type: Object,
+  },
+});
 </script>
-
 
 <template>
   <li class="flex space-x-6 py-6">
     <img
-        :src="item.image"
-        :alt="'Imagen de ' + item.name"
-        class="h-24 w-24 flex-none rounded-md"
-        />
+      :src="item.image"
+      :alt="'Imagen de ' + item.name"
+      class="h-24 w-24 flex-none rounded-md"
+    />
 
-        <div class="flex-auto space-y-2">
-            <h3 class="text-gray-900">{{ item.name }}</h3>
-            <p>{{ formatCurrency( item.price ) }}</p>
-        </div>
+    <div class="flex-auto space-y-2">
+      <h3 class="text-gray-900">{{ item.name }}</h3>
+      <p>{{ formatCurrency(item.price) }}</p>
+
+      <select
+      class="w-32  p-2 rounded-lg bg-white">
+
+        <option v-for="n in 5" :key="n" :value="n">{{ n }}</option>
+      
+    </select>
+
+    </div>
+
   </li>
 </template>
-
