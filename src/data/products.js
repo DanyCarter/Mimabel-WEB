3658ef32657f4527911eec9b0821d7ea764ba66a
Daniel <danielcadenas88@gmail.com>
2024-05-27 16:50:09 +0200

    Aumentando el stock

--- src/data/products.js
+++ src/data/products.js
@@ -239,10 +239,173 @@ export const products = [
       "availability": 10,
       "category": 3
     },
+    
     {
-      "name": "Sudadera xxl",
+      "name": "Camiseta Blanca Cold",
+      "price": 15,
+      "availability": 8,
+      "category": 4
+  },
+  {
+      "name": "Camiseta Negra Cold",
       "price": 12,
-      "availability": 13,
-      "category": 1
-    }
+      "availability": 5,
+      "category": 4
+  },
+  {
+      "name": "Camiseta Gris Cold",
+      "price": 18,
+      "availability": 7,
+      "category": 4
+  },
+  {
+      "name": "Camiseta Gris Luck",
+      "price": 14,
+      "availability": 10,
+      "category": 4
+  },
+  {
+      "name": "Camiseta Blanca Nike",
+      "price": 16,
+      "availability": 12,
+      "category": 4
+  },
+  {
+      "name": "Camiseta Negra",
+      "price": 13,
+      "availability": 6,
+      "category": 4
+  },
+  {
+      "name": "Camiseta Blanca",
+      "price": 17,
+      "availability": 9,
+      "category": 4
+  },
+  {
+      "name": "Camiseta Stussy x Nike",
+      "price": 11,
+      "availability": 4,
+      "category": 4
+  },
+  {
+      "name": "Camiseta Night & Strawberry",
+      "price": 19,
+      "availability": 11,
+      "category": 4
+  },
+  {
+      "name": "Camiseta ColdCulture",
+      "price": 10,
+      "availability": 3,
+      "category": 4
+  },
+  {
+    "name": "Camiseta Skate and Nike",
+    "price": 10,
+    "availability": 3,
+    "category": 4
+},
+  {
+      "name": "Bañador Mujer Verde",
+      "price": 12,
+      "availability": 6,
+      "category": 5
+  },
+  {
+      "name": "Bañador Hombre Naranja",
+      "price": 14,
+      "availability": 9,
+      "category": 5
+  },
+  {
+      "name": "Parte Inferior para Mujer",
+      "price": 10,
+      "availability": 4,
+      "category": 5
+  },
+  {
+      "name": "Parte Superior para Mujer",
+      "price": 16,
+      "availability": 8,
+      "category": 5
+  },
+  {
+      "name": "Bikini completo Azul",
+      "price": 18,
+      "availability": 7,
+      "category": 5
+  },
+  {
+      "name": "Bañador Blue banana",
+      "price": 13,
+      "availability": 5,
+      "category": 5
+  },
+  {
+      "name": "Bañador Azul Claro",
+      "price": 17,
+      "availability": 11,
+      "category": 5
+  },
+  {
+      "name": "Bañador Naranja Mujer",
+      "price": 15,
+      "availability": 10,
+      "category": 5
+  },
+  {
+      "name": "Bañador con estampado",
+      "price": 11,
+      "availability": 3,
+      "category": 5
+  },
+  {
+      "name": "Bañador kys",
+      "price": 8,
+      "availability": 6,
+      "category": 5
+  },
+  {
+      "name": "Bañador marron",
+      "price": 9,
+      "availability": 12,
+      "category": 5
+  },
+  {
+      "name": "Bañador Mujer Tropical",
+      "price": 16,
+      "availability": 14,
+      "category": 5
+  },
+  {
+      "name": "Calcetines Blancos Cold",
+      "price": 12,
+      "availability": 8,
+      "category": 6
+  },
+  {
+      "name": "Calcetines Cold",
+      "price": 14,
+      "availability": 10,
+      "category": 6
+  },
+  {
+      "name": "Calcetines Marrones",
+      "price": 11,
+      "availability": 7,
+      "category": 6
+  },
+  {
+      "name": "Calcetines Ocuros",
+      "price": 13,
+      "availability": 9,
+      "category": 6
+  },
+  {
+      "name": "Calcetines Azules",
+      "price": 10,
+      "availability": 5,
+      "category": 6
+  }
   ]
--- src/views/admin/SeederView.vue
+++ src/views/admin/SeederView.vue
@@ -1,107 +1,107 @@
 <script setup>
-  import {  ref, getDownloadURL, uploadBytesResumable  } from 'firebase/storage'
+  import { ref, getDownloadURL, uploadBytesResumable } from 'firebase/storage'
   import { collection, addDoc } from 'firebase/firestore'
-  import { useFirebaseStorage, useFirestore  } from 'vuefire'
+  import { useFirebaseStorage, useFirestore } from 'vuefire'
   import { products } from '../../data/products'
 
   const storage = useFirebaseStorage()
-
   const db = useFirestore();
 
-  async function seedDB()  {
-    
+  async function seedDB() {
     const productsCollection = collection(db, "products");
 
     for (let i = 1; i < products.length; i++) {
-          const url = await getDownloadURL( ref(storage, `products/producto${i}.jpg`) );
+      // Asume que tienes ambas versiones .webp y .jpg y eliges una para cada producto
+      const formats = ['webp', 'jpg'];
+      let url = '';
 
-          addDoc(productsCollection, { 
-              name: products[i - 1 ].name,
-              price: products[i - 1 ].price,
-              availability: products[i - 1 ].availability,
-              category: products[i - 1 ].category,
-              image: url
-          }).then(() => {
-            console.log("Producto Agregado...");
-          }).catch((error) => {
-            console.error(error);
-          });
+      for (let format of formats) {
+        try {
+          url = await getDownloadURL(ref(storage, `products/producto${i}.${format}`));
+          break; // Sal del bucle si el archivo se encuentra
+        } catch (error) {
+          console.log(`No se encontró producto${i}.${format}, intentando otro formato...`);
+        }
+      }
 
-          if(i === products.length - 1 ) {
-            alert('Se subieron los Productos')
-          }
+      if (url) {
+        addDoc(productsCollection, {
+          name: products[i - 1].name,
+          price: products[i - 1].price,
+          availability: products[i - 1].availability,
+          category: products[i - 1].category,
+          image: url
+        }).then(() => {
+          console.log("Producto Agregado...");
+        }).catch((error) => {
+          console.error(error);
+        });
+
+        if (i === products.length - 1) {
+          alert('Se subieron los Productos')
+        }
+      } else {
+        console.error(`No se encontró ningún archivo válido para producto${i}`);
+      }
     }
-
   }
 
   const handleSubmit = (data) => {
-
-    const urls = []
+    const urls = [];
     data.images.forEach(fileItem => {
-        const storageRef = ref(storage, '/products');
-        
-        // Upload the file to Firebase Storage
-        const uploadTask = uploadBytesResumable(ref(storageRef, fileItem.name), fileItem.file);
+      const storageRef = ref(storage, `products/${fileItem.name}`);
+      
+      // Upload the file to Firebase Storage
+      const uploadTask = uploadBytesResumable(storageRef, fileItem.file);
 
-        // Monitor the upload progress and retrieve the download URL
-        uploadTask.on('state_changed', snapshot => {
-          // Upload progress can be monitored here
-        }, error => {
-          console.log(error)
-        }, () => {
-            // Upload is complete, get the download URL
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                urls.push(downloadURL);
-                if (urls.length === data.images.length) {
-                   alert('Se subieron las imágenes')
-                }
-            });
+      // Monitor the upload progress and retrieve the download URL
+      uploadTask.on('state_changed', snapshot => {
+        // Upload progress can be monitored here
+      }, error => {
+        console.log(error)
+      }, () => {
+        // Upload is complete, get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          urls.push(downloadURL);
+          if (urls.length === data.images.length) {
+            alert('Se subieron las imágenes');
+          }
         });
-    }) 
-
-
-
+      });
+    })
   }
 </script>
 
-
 <template>
-    <div class="mt-10">
-        <Link
-            to="products"
+  <div class="mt-10">
+    <Link to="products">Volver</Link>
+
+    <h1 class="text-4xl my-10 font-extrabold">Seeder</h1>
+
+    <div class="flex justify-center bg-white shadow">
+      <div class="mx-auto mt-10 p-10 w-full 2xl:w-2/4">
+        <FormKit
+          type="form"
+          submit-label="Agregar Imágenes"
+          incomplete-message="No se pudo enviar, revisa los mensajes"
+          @submit="handleSubmit"
         >
-            Volver
-        </Link>
+          <FormKit 
+            type="file"
+            multiple="true"
+            name="images"
+          />
+        </FormKit>
 
-        <h1 class="text-4xl my-10 font-extrabold">Seeder</h1>
-
-        <div class="flex justify-center bg-white shadow">
-            <div class="mx-auto mt-10 p-10 w-full  2xl:w-2/4">
-                <FormKit
-                    type="form"
-                    submit-label="Agregar Imágenes"
-                    incomplete-message="No se pudo enviar, revisa los mensajes"
-                    @submit="handleSubmit"
-                > 
-
-                    <FormKit 
-                      type="file"
-                      multiple="true"
-                      name="images"
-                    />
-
-              </FormKit>
-
-              <div class="mt-16">
-                  <FormKit
-                      type="form"
-                      submit-label="Agregar Productos"
-                      incomplete-message="No se pudo enviar, revisa los mensajes"
-                      @submit="seedDB"
-                    > 
-                  </FormKit>
-                </div>
-            </div>
+        <div class="mt-16">
+          <FormKit
+            type="form"
+            submit-label="Agregar Productos"
+            incomplete-message="No se pudo enviar, revisa los mensajes"
+            @submit="seedDB"
+          />
         </div>
+      </div>
+    </div>
   </div>
 </template>
