8606b9462c78bb9a27124d66cf98aae6f13fc278
DanyCarter <danielcadenas88@gmail.com>
2024-06-05 13:52:37 +0200

    Actualizacion hash history

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import ShopView from '../views/ShopView.vue';
 import AdminLayout from '../views/admin/AdminLayout.vue';
 import { onAuthStateChanged } from 'firebase/auth';
@@ -74,7 +74,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
